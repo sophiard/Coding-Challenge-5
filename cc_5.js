6569e5a6c4c0f866e71f6946573a37a6289d8abc
Sophia Dolan <25srdolan@gmail.com>
2025-02-05 15:46:41 -0500

    Task 1 - Customer Profile

---
+++ cc_5.js
@@ -0,0 +1,10 @@
+// task 1
+const customer = {
+    name: "John Doe",
+    age: 35,
+    email: "john.doe@email.com"}; 
+    //declared an object customer
+  
+  console.log(`Name: ${customer.name}, Age: ${customer.age}, Email: ${customer.email}`);
+//log properties 
+
