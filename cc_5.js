08c561572c720ab387257a43155845d47a3d8ae9
Sophia Dolan <25srdolan@gmail.com>
2025-02-06 13:25:48 -0500

    Task 2 - Order Details

--- cc_5.js
+++ cc_5.js
@@ -8,3 +8,22 @@ const customer = {
   console.log(`Name: ${customer.name}, Age: ${customer.age}, Email: ${customer.email}`);
 //log properties 
 
+
+//Task 2 - Order Details.
+
+let order = {
+  orderId: 12345,
+  totalAmount: 150,
+  status: "Processing",
+  displayOrder: function() {
+      return `Order ID number: ${this.orderId}, Total Amount: $${this.totalAmount}, Status: ${this.status}`;
+  }
+};
+// I declared an object Order with propertiees and added a method called display order
+//I then returned the properties using this 
+
+console.log("Order details:", order);
+console.log("display order:", order.displayOrder());
+//loggging order and display order 
+
+// Declaring an inventory object
